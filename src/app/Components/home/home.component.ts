ccb09b4be9f738a0a6eaf711a6db104e9bcbd79a
Hemant Nag <104633997+dev-hemant63@users.noreply.github.com>
2024-06-02 17:17:58 +0000

    crud done

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../Services/product.service';
-import { ProductList } from '../../Interfaces/Product/ProductList';
+import { CreateProduct, ProductList, Products } from '../../Interfaces/Product/ProductList';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,28 +9,95 @@ import { ProductList } from '../../Interfaces/Product/ProductList';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  request: CreateProduct | undefined;
   data: ProductList = [];
+  productFrom: FormGroup | undefined;
+  productDetails: Products = {
+    id: 0,
+    title: '',
+    price: 0,
+    description: '',
+    category: '',
+    image: '',
+    rating: {
+      rate: 0,
+      count: 0
+    }
+  };
 
-  constructor(private _productService:ProductService){
+  constructor(private _productService: ProductService, private fb: FormBuilder) {
     this.getProducts();
+    this.productFrom = this.fb.group({
+      category: ['', Validators.required],
+      title: ['', Validators.required],
+      price: ['', Validators.required],
+      image: ['', Validators.required],
+    });
   }
 
-  getProducts(){
-    this._productService.getProductList().subscribe(response =>{
+  getProducts() {
+    this._productService.getProductList().subscribe(response => {
       this.data = response;
-    },error =>{
+    }, error => {
       console.error('Error fetching data:', error);
     });
-    
+
   }
-  deleteProduct(id:number){
-    this._productService.delete(id).subscribe(response =>{
-      if(response.id){
+  deleteProduct(id: number) {
+    this._productService.delete(id).subscribe(response => {
+      if (response.id) {
         alert('Product Deleted Successfully!');
         this.getProducts();
       }
-    },error =>{
+    }, error => {
       console.error('Error fetching data:', error);
     });
+  }
+  addProduct(requestData: CreateProduct) {
+      this.request = requestData;
+      if (!requestData.id) {
+        this._productService.addProduct(this.request).subscribe(apires => {
+          if (apires.id) {
+            alert('Product Add Successfully!');
+            this.getProducts();
+            this.closeModal();
+          }
+        });
+      }
+      else {
+        this._productService.updateProduct(this.request).subscribe(apires => {
+          if (apires.id) {
+            alert('Product Update Successfully!');
+            this.getProducts();
+            this.closeModal();
+          }
+        });
+      }
+
+  }
+  getProductDetails(productid: number) {
+    if (productid) {
+      this._productService.getProductById(productid).subscribe(apidata => {
+        this.productDetails = apidata;
+      }, error => {
+        console.error(error);
+      });
+    }
+
+  }
+
+  closeModal() {
+    const modalElement = document.getElementById('exampleModal');
+    if (modalElement) {
+      (modalElement as any).classList.remove('show');
+      modalElement.setAttribute('aria-hidden', 'true');
+      modalElement.removeAttribute('aria-modal');
+      modalElement.style.display = 'none';
+      document.body.classList.remove('modal-open');
+      const modalBackdrop = document.querySelector('.modal-backdrop');
+      if (modalBackdrop) {
+        modalBackdrop.remove();
+      }
+    }
   }
 }
